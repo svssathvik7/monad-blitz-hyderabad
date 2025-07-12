4d4e3f075ff4c8c6d8b532b1f5d9a06214433da1
SATHVIK SVS <svssathvik77@gmail.com>
2025-07-12 14:30:18 +0530

    write v1 contract

--- contract/src/Counter.sol
+++ contract/src/Counter.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+contract Test1 {
+    event TransferTo(
+        address indexed sender,
+        bytes32 indexed receiver, // use bytes32 for indexed
+        bytes32 indexed cid       // use bytes32 for indexed
+    );
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+    function transfer(string calldata receiver, string calldata cid) external {
+        // Hash the receiver and CID to bytes32 to emit as indexed params
+        bytes32 receiverHash = keccak256(abi.encodePacked(receiver));
+        bytes32 cidHash = keccak256(abi.encodePacked(cid));
 
-    function increment() public {
-        number++;
+        emit TransferTo(msg.sender, receiverHash, cidHash);
     }
 }
